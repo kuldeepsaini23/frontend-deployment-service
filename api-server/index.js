f6cf290ea7982691661e51f141923778715771b4
kuldeepsaini02311@gmail.com <kuldeepsaini02311@gmail.com>
2024-02-11 15:25:55 +0530

    Add ioredis dependency and update .env file

---
+++ api-server/index.js
@@ -0,0 +1,96 @@
+const express = require("express");
+const dotenv = require("dotenv");
+// Load environment variables
+dotenv.config();
+const { generateSlug } = require("random-word-slugs");
+const { ECSClient, RunTaskCommand, Scope } = require("@aws-sdk/client-ecs");
+const { Server } = require("socket.io");
+const {Redis} = require("ioredis");
+
+const app = express();
+const PORT = process.env.PORT || 9000;
+
+const subscriber = new Redis(
+  "rediss://default:AVNS_dFsy98x-cfeBvY0UGpA@redis-27238091-kuldeepsaini02311-43f7.a.aivencloud.com:17544"
+);
+
+const io = new Server({cors:'*'});
+
+io.on('connection', (socket) => {
+  socket.on('subscribe', channel => {
+    socket.join(channel);
+    socket.emit('message', `Subscribed/Joined to ${channel}`);
+  })
+});
+io.listen(9001, () => {console.log('Socket.io server is running on port 9001')});
+
+const config = {
+  CLUSTER: process.env.CLUSTER_ARN,
+  TASK: process.env.TASK_DEFINITION_ARN,
+};
+
+const ecsClient = new ECSClient({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+});
+
+// Middleware
+app.use(express.json());
+
+app.post("/project", async (req, res) => {
+  const { gitURL, slug } = req.body;
+  const projectSlug = slug ? slug : generateSlug();
+
+  // Spin the container
+  const command = new RunTaskCommand({
+    cluster: config.CLUSTER,
+    taskDefinition: config.TASK,
+    launchType: "FARGATE",
+    count: 1,
+    networkConfiguration: {
+      awsvpcConfiguration: {
+        assignPublicIp: "ENABLED",
+        subnets: [
+          process.env.SUBNET1,
+          process.env.SUBNET2,
+          process.env.SUBNET3,
+        ],
+        securityGroups: [process.env.SECURITY_GROUP],
+      },
+    },
+    overrides: {
+      // Yha pe environment variables set krne h
+      containerOverrides: [
+        {
+          name: process.env.TASK_DEFIANTION_NAME,
+          environment: [
+            { name: "GIT_REPOSITORY__URL", value: gitURL },
+            { name: "PROJECT_ID", value: projectSlug },
+          ],
+        },
+      ],
+    },
+  });
+
+  //Send the command to ECS
+  await ecsClient.send(command);
+  // return the response
+  return res.json({
+    status: "queued",
+    data: { projectSlug, url: `http://${projectSlug}.localhost:8000/` },
+  });
+});
+
+// Subscribe to the logs
+async function initRedisSubscriber() {
+  console.log("Subscribed to logs...");
+  subscriber.psubscribe('logs:*');
+  subscriber.on('pmessage', (pattern, channel, message)=>{
+    io.to(channel).emit('message', message);
+  })
+}
+initRedisSubscriber();
+app.listen(PORT, () => console.log(`API Server is running on port ${PORT}`));
--- build-server/script.js
+++ build-server/script.js
@@ -2,34 +2,35 @@ const { exec } = require("child_process");
 const path = require("path");
 const mime = require("mime-types");
 const dotenv = require("dotenv");
-
 // Load environment variables from .env file
 dotenv.config();
+const { Redis } = require("ioredis");
+
+
 // file system module
 const fs = require("fs");
 const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 
-
-// const s3Client = new S3Client({
-//   region: process.env.AWS_REGION,
-//   credentials: {
-//     accessKeyId: process.env.AWS_ACCESS_KEY,
-//     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-//   },
-// });
-
 const s3Client = new S3Client({
-  region: 'ap-south-1',
+  region: process.env.AWS_REGION,
   credentials: {
-      accessKeyId: 'AKIA4SGZZAHUZG7YVAHG',
-      secretAccessKey: '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'
-  }
-})
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+});
+
+
+const publisher = new Redis(process.env.REDIS_HOST);
 
 const PROJECT_ID = process.env.PROJECT_ID;
 
+function publishLog(log) {
+  publisher.publish(`logs:${PROJECT_ID}`, JSON.stringify(log));
+}
+
 async function init() {
   console.log("Executing Script.js");
+  publishLog("Build Started...");
   // __dirname(/home/app/) is the directory of the current file
   const outDirPath = path.join(__dirname, "output");
   // go to the output directory and install the dependencies and build the project
@@ -39,25 +40,31 @@ async function init() {
   p.stdout.on("data", function (data) {
     // The data event is fired when the process writes to its stdout
     //* This is a Buffer object
-    console.log(data);
+    console.log(data.toString());
+    publishLog(data.toString());
   });
 
   // On error
   p.stdout.on("error", function (data) {
     //* This is a Buffer object
     console.log("Error: ", data.toString());
+    publishLog("Error: ", data.toString());
   });
 
   // On close
   p.stdout.on("close", async function (data) {
     //* This is a Buffer object
     console.log("Build Successfull🎉🥳🥳🎉");
+    publishLog("Build Successfull🎉🥳🥳🎉");
     // console.log("Close: ", data.toString());
-    const distFolderPath = path.join(__dirname, 'output', 'dist')
+    const distFolderPath = path.join(__dirname, "output", "dist");
     // get the content of the dist folder(Array)
     const distFolderContent = fs.readdirSync(distFolderPath, {
       recursive: true,
     });
+
+    publishLog("Starting upload...");
+
     for (const file of distFolderContent) {
       //! Yha pe glti ki hai filepath calculate krna hai
       const filePath = path.join(distFolderPath, file);
@@ -67,19 +74,21 @@ async function init() {
       }
 
       console.log(`Uploading ${filePath} to S3`);
+      publishLog(`Uploading ${filePath} to S3`);
       const command = new PutObjectCommand({
-        // Bucket: process.env.BUCKET_NAME,
-        Bucket:'vercel-clone-kuldeep',
+        Bucket: process.env.BUCKET_NAME,
+        // Bucket: "vercel-clone-kuldeep",
         Key: `__outputs/${PROJECT_ID}/${file}`,
         Body: fs.createReadStream(filePath),
         ContentType: mime.lookup(filePath),
       });
       await s3Client.send(command);
       console.log(`Uploaded ${filePath} to S3`);
+      publishLog(`Uploaded ${filePath} to S3`);
     }
     console.log("Done uploading files to S3🎉🥳🥳🎉");
+    publishLog("Done uploading files to S3🎉🥳🥳🎉");
   });
 }
-
 
 init();
